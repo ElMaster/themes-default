f8f48035ae47c19d8f8a83652c180c2b9dae2cc4
ElMaster <roman.kosjk@gmail.com>
2016-12-09 15:56:36 +0200

    Add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-$('#top-slider').slick({
-    autoplay: true,
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1
-});
-$('#footer-slider').slick({
+$(document).ready(function() {
+    $('#top-slider').slick({
+        autoplay: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1
+    });
+    $('#footer-slider').slick({
         autoplay: true,
         dots: false,
         infinite: true,
@@ -37,11 +38,44 @@ $('#footer-slider').slick({
             // instead of a settings object
         ]
     });
-$('.slider-hor').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    dots: true,
-    autoplay: false
-    //autoplaySpeed: 3000
+    $('.slider-hor').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        autoplay: false
+        //autoplaySpeed: 3000
+    });
+    $('.slider-content-hor').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    
+    $(".fancy").fancybox();
 });
