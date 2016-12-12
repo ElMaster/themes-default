bcf93139f91614ebd49d5fd80d1f3591d0e1064d
ElMaster <roman.kosjk@gmail.com>
2016-12-12 14:29:23 +0200

    ff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,78 @@
+//multiple text input
+(function($) {
+    var defaults = {
+        auto:3000
+    };
+    $.fn.context_hints = function(params) {
+        var options = $.extend({}, defaults, params);
+        $(this).each(function() {
+            var $this = $(this),
+                $navA = $this.find("div.nav a"),
+                $backward = $this.find("div.nav a.backward"),
+                $forward = $this.find("div.nav a.forward"),
+                $li = $this.find("li"),
+                $active = $this.find("li.active"),
+                $next = $active.next("li"),
+                $prev = $active.prev("li");
+
+            $forward.click(function(e) {
+                $active.removeClass("active")
+                if(!$next.is("li")) {
+                    $next = $($li[0]);
+                }
+                $active = $next.addClass("active");
+                getNext();
+                getPrev();
+
+                if(options.auto) {
+                    clearInterval(hintsInterval);
+                    hintsInterval = setInterval(function() {interval();}, options.auto);
+                }
+                e.stopPropagation();
+                e.preventDefault();
+            });
+
+            $backward.click(function(e) {
+                $active.removeClass("active");
+
+                if(!$prev.is("li")) {
+                    
+                    $prev = $($li[$li.length - 1]);
+
+                }
+                $active = $prev.addClass("active");
+                getNext();
+                getPrev();
+
+                if(options.auto) {
+                    clearInterval(hintsInterval);
+                    hintsInterval = setInterval(function() {interval();}, options.auto);
+                }
+                e.stopPropagation();
+                e.preventDefault();
+            });
+
+            function interval() {
+                $forward.click();
+            }
+
+            function getNext() {
+                $next = $active.next("li");
+            }
+
+            function getPrev() {
+                $prev = $active.prev("li");
+            }
+
+            if(options.auto) {//auto click
+                var hintsInterval = setInterval(function() {interval();}, options.auto);
+            }
+        });
+        return this;
+    };
+})(jQuery);
+
+
 $(document).ready(function() {
     $('#top-slider').slick({
         autoplay: true,
@@ -76,6 +151,19 @@ $(document).ready(function() {
             // instead of a settings object
         ]
     });
-    
-    $(".fancy").fancybox();
+    if($(".fancy").length) {
+        $(".fancy").fancybox({
+            helpers : {
+                title : {
+                    type : 'inside'
+                }
+            }
+        });
+    }
+
+    // $(".CONTEXT-st").slick({
+    //     speed: 0,
+    //     fade: true
+    // })
+    $("#context_hints").context_hints({auto:7000});
 });
